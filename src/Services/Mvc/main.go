4ad343c3031f37bd31e6e29f4cee6500571c429f
Jasper Spahl <jasperspahl@web.de>
2022-12-20 03:40:13 +0100

    18 MVC microservice

--- src/Services/Mvc/main.go
+++ src/Services/Mvc/main.go
@@ -1,33 +1,33 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"os"
+	"regexp"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/librecuisine/mvc/dtos"
 )
 
-type Config struct{
+type Config struct {
 	ServerUrl string
 }
 
 func main() {
 	config := Config{}
-	serverUrl, ok := os.LookupEnv("SERVER_URL");
+	serverUrl, ok := os.LookupEnv("SERVER_URL")
 	if ok {
 		config.ServerUrl = serverUrl
 	} else {
 		config.ServerUrl = "http://localhost:5200"
 	}
 	r := gin.Default()
-	r.LoadHTMLGlob("templates/*")
-	r.GET("/ping", func (c *gin.Context)  {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	r.Static("/static", "./static")
+	r.LoadHTMLGlob("templates/**/*")
 	r.GET("/", func(c *gin.Context) {
 		var recipes []dtos.Recipe
 		resp, err := http.Get(config.ServerUrl + "/r/api/Recipes")
@@ -40,7 +40,7 @@ func main() {
 			c.Status(http.StatusInternalServerError)
 			return
 		}
-		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+		c.HTML(http.StatusOK, "index.gohtml", gin.H{
 			"Recipes": recipes,
 		})
 	})
@@ -57,8 +57,56 @@ func main() {
 			c.Status(http.StatusInternalServerError)
 			return
 		}
-		c.HTML(http.StatusOK, "recipe.tmpl", recipe)
+		c.HTML(http.StatusOK, "recipe.gohtml", recipe)
 
 	})
-	r.Run();
+	r.GET("/create", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "create.gohtml", nil)
+	})
+	r.POST("/create", func(c *gin.Context) {
+		var recipe dtos.RecipeCreate
+		err := c.Bind(&recipe)
+		if err != nil {
+			fmt.Printf("error binding recipe: %v", err)
+			c.Status(http.StatusBadRequest)
+			return
+		}
+		var recipeDto dtos.Recipe
+		recipeDto.Name = recipe.Name
+		recipeDto.Desc = recipe.Desc
+		re := regexp.MustCompile(`([0-9]+) (\w+) (.*)`)
+		for _, ingredient := range recipe.Ingredients {
+			var ingredientDto dtos.Ingredient
+			matches := re.FindStringSubmatch(ingredient)
+			if len(matches) != 4 {
+				fmt.Printf("error parsing ingredient: %v", ingredient)
+				c.Status(http.StatusBadRequest)
+				return
+			}
+			ingredientDto.Amount, err = strconv.ParseFloat(matches[1], 64)
+			if err != nil {
+				fmt.Printf("error parsing ingredient amount: %v", ingredient)
+				c.Status(http.StatusBadRequest)
+				return
+			}
+			ingredientDto.Unit = matches[2]
+			ingredientDto.Name = matches[3]
+			recipeDto.Ingredients = append(recipeDto.Ingredients, ingredientDto)
+		}
+		recipeDto.Steps = recipe.Steps
+		jsonRecipe, err := json.Marshal(recipeDto)
+		if err != nil {
+			fmt.Printf("error marshalling recipe: %v", err)
+			c.Status(http.StatusInternalServerError)
+			return
+		}
+		_, err = http.Post(config.ServerUrl+"/r/api/Recipes", "application/json", bytes.NewBuffer(jsonRecipe))
+		if err != nil {
+			fmt.Printf("error posting recipe: %v", err)
+			c.Status(http.StatusBadRequest)
+			return
+		}
+		c.Redirect(http.StatusFound, "/")
+	})
+	r.Run()
 }
